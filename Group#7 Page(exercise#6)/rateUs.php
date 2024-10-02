3bfd6efb189ef7c9b454d268301f171e598d924a
nurfaisahadjisalic <nurfaisa609@gmail.com>
2024-10-01 18:03:58 -0700

    Add files via upload

---
+++ Group#7 Page(exercise#6)/rateUs.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="group.css"> 
+    <title>Rate Us</title>
+
+</head>
+<body>
+
+<?php include('C:\Users\JUAN KIAN C. MOLDEZ\OneDrive\Desktop\Group#7 Page\header.php'); ?>
+    
+    
+
+
+
+</body>
+</html>
